c6a946c3d8e6020798f0aa8c6e3576a59baa6edc
Shivam Verma <shivam.bitsgoa@gmail.com>
2012-10-27 22:30:48 +0530

    added proxy support and code restructuring for imdb.py

--- imdb.py
+++ imdb.py
@@ -1,30 +1,70 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import re
+import sys
 
-def imdb(movie_name):
-    movie_name= movie_name.split()
-    movie_url='+'.join(movie_name)
-    url_pref = "http://www.imdb.com/find?q="
-    url_suf = "&s=all"
-    imdb_url = url_pref+movie_url+url_suf;
-    print imdb_url
-    source = urllib2.urlopen(imdb_url).read()
-    title = re.search(r'/title/tt[0-9]*/', source)
-    movie_url = "http://imdb.com"+title.group()
-    movie_source = urllib2.urlopen(movie_url).read()
-    print "Movie Page Fetched"
-    soup = BeautifulSoup(movie_source)
-    rating = soup.findAll("span", itemprop="ratingValue")
-    for node in rating:
-        print "Rating: " + (''.join(node.findAll(text=True)))+"/10"
-    voters = soup.findAll("span", itemprop="ratingCount")
-    for node in voters:
-        print "Number of Voters: "+''.join(node.findAll(text=True))
-    desc = soup.findAll("p", itemprop="description")
-    for node in desc:
-        print "Description: "+(''.join(node.findAll(text=True)))
+def parse_tag(tag_to_parse):
+        index_begin = tag_to_parse.find(">")
+        index_end = tag_to_parse.find("<", index_begin)
+        return tag_to_parse[index_begin+1:index_end] 
 
-movie_name = raw_input()
-imdb(movie_name)
+class Imdb:
+    def __init__(self, name):
+        name = name.split()
+        url = '+'.join(name)
+        prefix = "http://www.imdb.com/find?q="
+        suffix = "&s=all"
+        search_url = prefix+url+suffix;
+
+        http_proxy_full_auth_string = "http://%s:%s@%s:%s" % ('username','password','proxy_server','port')
+        proxy_handler = urllib2.ProxyHandler({"http": http_proxy_full_auth_string})
+        opener = urllib2.build_opener(proxy_handler)
+        urllib2.install_opener(opener)
+        search_result = urllib2.urlopen(search_url).read()
+        
+        title = re.search(r'/title/tt[0-9]*/', search_result)
+        movie_url = "http://imdb.com"+title.group(0)
+        self.movie_source = urllib2.urlopen(movie_url).read()
+
+        self.soup = BeautifulSoup(self.movie_source)
+    
+    def get_rating(self):
+        self.rating = self.soup.findAll("span", itemprop="ratingValue")
+        for i in self.rating:
+            print (''.join(i.findAll(text=True)))
+
+    def get_voters(self):
+        self.voters = self.soup.findAll("span", itemprop="ratingCount")
+        for i in self.voters:
+            print (''.join(i.findAll(text=True)))
+
+    def get_directors(self):
+        self.directors = self.soup.findAll("a", itemprop="director")
+        for i in self.directors:
+            print (''.join(i.findAll(text=True)))
+
+    def get_actors(self):
+        self.actors = self.soup.findAll("a", itemprop="actors")
+        for i in self.actors:
+            print (''.join(i.findAll(text=True)))
+
+    def get_storyline(self):
+        index_begin = self.movie_source.find("<h2>Storyline</h2>")
+        index_storyline = self.movie_source.find("<p>", index_begin)
+        index_end = self.movie_source.find("<em", index_storyline+1)
+        print self.movie_source[(index_storyline+3):index_end]
+
+movie_name = sys.stdin.readline()
+imdb = Imdb(movie_name)
+print "\nRating :" 
+imdb.get_rating()
+print "\nVoters :"
+imdb.get_voters()
+print "\nDirectors :"
+imdb.get_directors()
+print "\nActors :"
+imdb.get_actors()
+print "\nStoryline :"
+imdb.get_storyline()
+
 
